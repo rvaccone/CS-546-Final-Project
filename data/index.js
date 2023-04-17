3bf1dfc3c7700c1b83e665d9806f7aff9455d8e3
Jack Harrington <jackharrington1117@gmail.com>
2023-04-17 01:10:40 -0400

    Added basic route formatting and the index files for data and routes.

---
+++ data/index.js
@@ -0,0 +1,11 @@
+import * as users_functions from '../data/users.js';
+import * as games_functions from '../data/games.js';
+import * as games_members_functions from '../data/gameMembers.js';
+import * as courts_functions from '../data/courts.js';
+import * as court_rev_functions from '../data/courtReviews.js';
+
+export const usersData = users_functions;
+export const gamesData = games_functions;
+export const gameMembersData = games_members_functions;
+export const courtsData = courts_functions;
+export const courtReviewsData = court_rev_functions;
---
+++ routes/index.js
@@ -0,0 +1,18 @@
+import userRoutes from './users.js';
+// import gameRoutes from './games.js';
+// import courtRoutes from './courts.js';
+
+const constructorMethod = (app) => {
+	// app.get('/', (req, res) => {
+	//  TODO: Add view page to this render.
+	//  res.render()
+	// })
+	app.use('/user', userRoutes);
+	// app.use('/game', gameRoutes);
+	// app.use('/court', courtRoutes);
+	app.use('*', (req, res) => {
+		res.status(404).json({ error: 'Route Not found' });
+	});
+};
+
+export default constructorMethod;
