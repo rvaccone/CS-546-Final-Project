d8a22517c840b311d0db1e64746b4c3806fdb595
saipranav789 <113039239+saipranav789@users.noreply.github.com>
2023-04-25 09:57:29 -0400

    Able to render maps using google maps api
    We are rendering only with maps embed api because any other form of the api call will be charged to my card.

--- routes/games.js
+++ routes/games.js
@@ -1,58 +1,65 @@
-import { Router } from 'express';
+import { Router } from "express";
 const router = Router();
-import { gamesData } from '../data/index.js';
-import * as validation from '../validation.js';
+import { gamesData } from "../data/index.js";
+import * as validation from "../validation.js";
 
 // TODO: render instead of json. Including data you want to render with it.
 
 router
-	.route('/:courtID')
+  .route("/:courtID")
 
-	// Create Game by courtID
-	.post(async (req, res) => {
-		const courtsPostData = req.body;
+  // Create Game by courtID
+  .post(async (req, res) => {
+    const courtsPostData = req.body;
 
-		// Checks to see if the req.body is empty.
-		if (!courtsPostData || Object.keys(courtsPostData).length === 0) {
-			return res.status(400).json({ error: 'There are no fields in the request body' });
-		}
+    // Checks to see if the req.body is empty.
+    if (!courtsPostData || Object.keys(courtsPostData).length === 0) {
+      return res
+        .status(400)
+        .json({ error: "There are no fields in the request body" });
+    }
 
-		// Checks to see if the correct number of fields were returned.
-		if (Object.keys(courtsPostData).length !== 5) {
-			return res.status(400).json({ error: 'The schema does not match the database.' });
-		}
+    // Checks to see if the correct number of fields were returned.
+    if (Object.keys(courtsPostData).length !== 5) {
+      return res
+        .status(400)
+        .json({ error: "The schema does not match the database." });
+    }
 
-		// Validates the input.
-		try {
-			courtsPostData.courtID = validation.checkID(courtsPostData.courtID, 'courtID');
-			courtsPostData.date = validation.checkDate(courtsPostData.date, 'date');
-			courtsPostData.time = validation.checkTime(courtsPostData.time, 'time');
-			courtsPostData.maxPlayers = validation.checkMaxPlayer(
-				courtsPostData.maxPlayers,
-				'maxPlayers'
-			);
-		} catch (e) {
-			console.log(e);
-			return res.status(400).json({ error: e });
-		}
+    // Validates the input.
+    try {
+      courtsPostData.courtID = validation.checkID(
+        courtsPostData.courtID,
+        "courtID"
+      );
+      courtsPostData.date = validation.checkDate(courtsPostData.date, "date");
+      courtsPostData.time = validation.checkTime(courtsPostData.time, "time");
+      courtsPostData.maxPlayers = validation.checkMaxPlayer(
+        courtsPostData.maxPlayers,
+        "maxPlayers"
+      );
+    } catch (e) {
+      console.log(e);
+      return res.status(400).json({ error: e });
+    }
 
-		// Creates a new game.
-		try {
-			console.log(courtsPostData);
-			const { courtID, date, time, maxPlayers } = courtsPostData;
-			const newGame = await gamesData.create(courtID, date, time, maxPlayers);
-			return res.status(200).json(newGame);
-		} catch (e) {
-			console.log(e);
-			return res.status(500).json({ error: e });
-		}
-	});
+    // Creates a new game.
+    try {
+      console.log(courtsPostData);
+      const { courtID, date, time, maxPlayers } = courtsPostData;
+      const newGame = await gamesData.create(courtID, date, time, maxPlayers);
+      return res.status(200).json(newGame);
+    } catch (e) {
+      console.log(e);
+      return res.status(500).json({ error: e });
+    }
+  });
 
 router
-	.route('/:gameID')
+  .route("/:gameID")
 
-	// Delete Game by gameID
-	.delete(async (req, res) => {})
+  // Delete Game by gameID
+  .delete(async (req, res) => {})
 
-	// Add player to game by gameID using create gameMember.
-	.put(async (req, res) => {});
+  // Add player to game by gameID using create gameMember.
+  .put(async (req, res) => {});
