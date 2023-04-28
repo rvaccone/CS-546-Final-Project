db125e70bfc5d19167a2e57015c173493ca8550f
saipranav789 <113039239+saipranav789@users.noreply.github.com>
2023-04-27 21:30:32 -0400

    Added files
    I have added search results handlebars, rendered pick up games and made some bug fixes

--- routes/courts.js
+++ routes/courts.js
@@ -1,41 +1,49 @@
-import { Router } from 'express';
+import { Router } from "express";
 const router = Router();
-import { courtsData } from '../data/index.js';
-import * as validation from '../validation.js';
+import { courtsData, gamesData, gameMembersData } from "../data/index.js";
+import * as validation from "../validation.js";
 
-router.route('/:id').get(async (req, res) => {
-	// Store the id from the url
-	let id = req.params.id;
+router.route("/:id").get(async (req, res) => {
+  // Store the id from the url
+  let id = req.params.id;
 
-	// Validate the id
-	try {
-		id = validation.checkID(id, 'id');
-	} catch (e) {
-		return res.status(400).render('Error', { errorMessage: e });
-	}
+  // Validate the id
+  try {
+    id = validation.checkID(id, "id");
+  } catch (e) {
+    return res.status(400).render("Error", { errorMessage: e });
+  }
 
-	// Get the court details
-	let courtDetails = null;
-	try {
-		courtDetails = await courtsData.get(id);
-	} catch (e) {
-		return res.status(400).render('Error', { errorMessage: e });
-	}
+  // Get the court details
+  let courtDetails = null;
+  try {
+    courtDetails = await courtsData.get(id);
+  } catch (e) {
+    return res.status(400).render("Error", { errorMessage: e });
+  }
+  let pickUpGames = null;
+  try {
+    pickUpGames = await gamesData.getAll(id);
+    console.log("gotem", pickUpGames);
+  } catch (e) {
+    return res.status(400).render("Error", { errorMessage: e });
+  }
 
-	// Check that the court exists
-	if (!courtDetails)
-		return res
-			.status(400)
-			.render('Error', { errorMessage: 'Error: Court not found' });
+  // Check that the court exists
+  if (!courtDetails)
+    return res
+      .status(400)
+      .render("Error", { errorMessage: "Error: Court not found" });
 
-	// Render the courtByID page with the court details
-	res.render('courtById', {
-		courtName: courtDetails.name,
-		address: courtDetails.location,
-		overallRating: courtDetails.overallRating,
-		latitude: courtDetails.lat,
-		longitude: courtDetails.long,
-	});
+  // Render the courtByID page with the court details
+  res.render("courtById", {
+    courtName: courtDetails.name,
+    address: courtDetails.location,
+    overallRating: courtDetails.overallRating,
+    latitude: courtDetails.lat,
+    longitude: courtDetails.long,
+    pickUpGames: pickUpGames,
+  });
 });
 
 export default router;
--- routes/games.js
+++ routes/games.js
@@ -1,142 +1,155 @@
-import { Router } from 'express';
+import { Router } from "express";
 const router = Router();
-import { gamesData } from '../data/index.js';
-import * as validation from '../validation.js';
+import { gamesData, usersData } from "../data/index.js";
+import * as validation from "../validation.js";
 
 // TODO: render instead of json. Including data you want to render with it.
 
 router
-	.route('/:courtID')
+  .route("/:courtID")
 
-	// Create Game by courtID
-	.post(async (req, res) => {
-		const courtsPostData = req.body;
+  // Create Game by courtID
+  .post(async (req, res) => {
+    const courtsPostData = req.body;
 
-		// Checks to see if the req.body is empty.
-		if (!courtsPostData || Object.keys(courtsPostData).length === 0) {
-			return res.status(400).render('Error', {
-				errorMessage: 'Error: There are no fields in the request body',
-			});
-		}
+    // Checks to see if the req.body is empty.
+    if (!courtsPostData || Object.keys(courtsPostData).length === 0) {
+      return res.status(400).render("Error", {
+        errorMessage: "Error: There are no fields in the request body",
+      });
+    }
 
-		// Checks to see if the correct number of fields were returned.
-		if (Object.keys(courtsPostData).length !== 5) {
-			return res.status(400).render('Error', {
-				errorMessage: 'Error: The schema does not match the database',
-			});
-		}
+    // Checks to see if the correct number of fields were returned.
+    if (Object.keys(courtsPostData).length !== 5) {
+      return res.status(400).render("Error", {
+        errorMessage: "Error: The schema does not match the database",
+      });
+    }
 
-		// Validates the input.
-		try {
-			courtsPostData.courtID = validation.checkID(courtsPostData.courtID, 'courtID');
-			courtsPostData.date = validation.checkDate(courtsPostData.date, 'date');
-			courtsPostData.time = validation.checkTime(courtsPostData.time, 'time');
-			courtsPostData.maxPlayers = validation.checkMaxPlayer(
-				courtsPostData.maxPlayers,
-				'maxPlayers'
-			);
-		} catch (e) {
-			console.log(e);
-			return res.status(400).render('Error', { errorMessage: e });
-		}
+    // Validates the input.
+    try {
+      courtsPostData.courtID = validation.checkID(
+        courtsPostData.courtID,
+        "courtID"
+      );
+      courtsPostData.date = validation.checkDate(courtsPostData.date, "date");
+      courtsPostData.time = validation.checkTime(courtsPostData.time, "time");
+      courtsPostData.maxPlayers = validation.checkMaxPlayer(
+        courtsPostData.maxPlayers,
+        "maxPlayers"
+      );
+    } catch (e) {
+      console.log(e);
+      return res.status(400).render("Error", { errorMessage: e });
+    }
 
-		// Creates a new game.
-		try {
-			console.log(courtsPostData);
-			const { courtID, date, time, maxPlayers } = courtsPostData;
-			const newGame = await gamesData.create(courtID, date, time, maxPlayers);
-			return res.status(200).json(newGame);
-		} catch (e) {
-			return res.status(400).render('Error', { errorMessage: e });
-		}
-	});
+    // Creates a new game.
+    try {
+      console.log(courtsPostData);
+      const { courtID, date, time, maxPlayers } = courtsPostData;
+      const newGame = await gamesData.create(courtID, date, time, maxPlayers);
+      return res.status(200).json(newGame);
+    } catch (e) {
+      return res.status(400).render("Error", { errorMessage: e });
+    }
+  });
 
 router
-	.route('/:gameID')
+  .route("/:gameID")
 
-	.get(async (req, res) => {
-		// Store the gameID from the url
-		let gameID = req.params.gameID;
+  .get(async (req, res) => {
+    // Store the gameID from the url
+    let gameID = req.params.gameID;
 
-		// Validate the gameID
-		try {
-			gameID = validation.checkID(gameID, 'gameID');
-		} catch (e) {
-			return res.status(400).render('Error', { errorMessage: e });
-		}
+    // Validate the gameID
+    try {
+      gameID = validation.checkID(gameID, "gameID");
+    } catch (e) {
+      return res.status(400).render("Error", { errorMessage: e });
+    }
 
-		// Get the game using the gameID
-		let gameDetails = null;
-		try {
-			gameDetails = await gamesData.get(gameID);
-		} catch (e) {
-			return res.status(400).render('Error', { errorMessage: e });
-		}
+    // Get the game using the gameID
+    let gameDetails = null;
+    try {
+      gameDetails = await gamesData.get(gameID);
+    } catch (e) {
+      return res.status(400).render("Error", { errorMessage: e });
+    }
+    let Host = await usersData.getFullName(gameDetails.userID.toString());
+    // Check that the game exists
+    if (!gameDetails)
+      return res
+        .status(400)
+        .render("Error", { errorMessage: "Error: Game not found" });
 
-		// Check that the game exists
-		if (!gameDetails)
-			return res
-				.status(400)
-				.render('Error', { errorMessage: 'Error: Game not found' });
+    // Return the game details
+    return res
+      .status(200)
+      .render("pickUpGame", { gameDetails: gameDetails, Host: Host });
+  })
 
-		// Return the game details
-		return res.status(200).json(gameDetails);
-	})
+  // Delete Game by gameID
+  .delete(async (req, res) => {
+    // Validate the gameID
+    try {
+      req.params.gameID = validation.checkID(req.params.gameID, "gameID");
+    } catch (e) {
+      return res.status(400).render("Error", { errorMessage: e });
+    }
 
-	// Delete Game by gameID
-	.delete(async (req, res) => {
-		// Validate the gameID
-		try {
-			req.params.gameID = validation.checkID(req.params.gameID, 'gameID');
-		} catch (e) {
-			return res.status(400).render('Error', { errorMessage: e });
-		}
+    // Call the delete data function for the game
+    try {
+      let deletedGame = await gamesData.remove(req.params.gameID);
+    } catch (e) {
+      return res.status(400).render("Error", { errorMessage: e });
+    }
+  })
 
-		// Call the delete data function for the game
-		try {
-			let deletedGame = await gamesData.remove(req.params.gameID);
-		} catch (e) {
-			return res.status(400).render('Error', { errorMessage: e });
-		}
-	})
+  // Add player to game by gameID using create gameMember.
+  .put(async (req, res) => {
+    // Get the request body
+    const gameMemberPutData = req.body;
 
-	// Add player to game by gameID using create gameMember.
-	.put(async (req, res) => {
-		// Get the request body
-		const gameMemberPutData = req.body;
+    // Checks to see if the req.body is empty
+    if (!bandInfo || Object.keys(bandInfo).length === 0)
+      return res
+        .status(400)
+        .render("Error", { error: "Error: The request was empty" });
 
-		// Checks to see if the req.body is empty
-		if (!bandInfo || Object.keys(bandInfo).length === 0)
-			return res.status(400).render('Error', { error: 'Error: The request was empty' });
+    // Check to see if the correct number of fields were returned
+    if (Object.keys(gameMemberPutData).length !== 2)
+      return res.status(400).render("Error", {
+        errorMessage: "The request contained the wrong number of fields",
+      });
 
-		// Check to see if the correct number of fields were returned
-		if (Object.keys(gameMemberPutData).length !== 2)
-			return res.status(400).render('Error', {
-				errorMessage: 'The request contained the wrong number of fields',
-			});
+    // Validate the IDs
+    try {
+      gameMemberPutData.gameID = validation.checkID(
+        gameMemberPutData.gameID,
+        "gameID"
+      );
+      gameMemberPutData.userID = validation.checkID(
+        gameMemberPutData.userID,
+        "userID"
+      );
+    } catch (e) {
+      return res.status(400).render("Error", { errorMessage: e });
+    }
 
-		// Validate the IDs
-		try {
-			gameMemberPutData.gameID = validation.checkID(gameMemberPutData.gameID, 'gameID');
-			gameMemberPutData.userID = validation.checkID(gameMemberPutData.userID, 'userID');
-		} catch (e) {
-			return res.status(400).render('Error', { errorMessage: e });
-		}
+    // Call the create gameMember create data function
+    try {
+      // Variable to store the return value of the create gameMember function
+      const createGameMemberData = await gamesData.createGameMember(
+        gameMemberPutData.gameID,
+        gameMemberPutData.userID
+      );
 
-		// Call the create gameMember create data function
-		try {
-			// Variable to store the return value of the create gameMember function
-			const createGameMemberData = await gamesData.createGameMember(
-				gameMemberPutData.gameID,
-				gameMemberPutData.userID
-			);
+      // Checking if the create gameMember function was successful
+      if (!createGameMemberData)
+        throw `Error: Could not add user to game with ID ${gameMemberPutData.gameID}`;
+    } catch (e) {
+      return res.status(400).render("Error", { errorMessage: e });
+    }
+  });
 
-			// Checking if the create gameMember function was successful
-			if (!createGameMemberData)
-				throw `Error: Could not add user to game with ID ${gameMemberPutData.gameID}`;
-		} catch (e) {
-			return res.status(400).render('Error', { errorMessage: e });
-		}
-    });
-    
 export default router;
