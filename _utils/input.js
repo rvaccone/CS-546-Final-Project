730283d48b1c07013f8f368ea7ab4240b1147022
Rocco <rvaccone@skiff.com>
2023-05-03 00:23:04 -0400

    Create input.js
    Added a file to deal with repeated functions for input variables. The first function will help with XSS protections

---
+++ _utils/input.js
@@ -0,0 +1,10 @@
+import xss from 'xss';
+
+// Function to apply xss protection to every value in an object
+const xssProtectObject = (obj) => {
+	for (let key in obj) obj[key] = xss(obj[key]);
+	return obj;
+};
+
+
+export { xssProtectObject };
