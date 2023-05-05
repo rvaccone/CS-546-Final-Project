b1253bff252ca609c99e6a14de0e40197e23bf73
Rocco <rvaccone@skiff.com>
2023-05-05 14:59:36 -0400

    Reverted pickup games

--- public/js/pickUpGames.js
+++ public/js/pickUpGames.js
@@ -1,32 +1,10 @@
-// Import the validation functions
-import * as validation from '../../validation.js';
-
-// Get the unjoin button
 const unjoinButton = document.getElementById('unjoinButton');
 
-// Create an array to hold the errors
-let errors = [];
-
-// Check that the button exists
 if (unjoinButton) {
-	// Create an array to hold the errors
-	errors = [];
-
-	// Add an event listener for the unjoin button
-	unjoinButton.addEventListener('click', async (event) => {
-		// Get the game ID from the URL
+	unjoinButton.addEventListener('click', async () => {
 		const currPath = window.location.pathname;
 		const gameId = currPath.replace('/game/gameDetails/', '');
 		console.log(gameId);
-
-		// Validate the game ID
-		try {
-			validation.checkID(gameId, 'gameID');
-		} catch (e) {
-			errors.push(e);
-		}
-
-		// Send a DELETE request to the server
 		try {
 			const response = await fetch(`/game/addUser/${gameId}`, {
 				method: 'DELETE',
@@ -37,13 +15,6 @@ if (unjoinButton) {
 			window.location.reload();
 		} catch (error) {
 			console.log(error);
-			errors.push(e);
-		}
-
-		// If there are errors, prevent the form from submitting and display the errors
-		if (errors.length > 0) {
-			// Prevent the form from submitting
-			event.preventDefault();
 		}
 	});
 }
