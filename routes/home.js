bd459d4ce11e4a7176a94863ac66d3f2be1698ad
Rocco <rvaccone@skiff.com>
2023-05-05 21:57:01 -0400

    Updated home index
    Limited the trending games to only four and check for user session before updating location

--- routes/home.js
+++ routes/home.js
@@ -1,51 +1,58 @@
-import { Router } from "express";
+import { Router } from 'express';
 const router = Router();
-import { gamesData } from "../data/index.js";
+import { gamesData } from '../data/index.js';
+import { checkUserSession } from '../utils/session.js';
 
 // Main Route
 router
-  .route("/")
+	.route('/')
+	// Get the homepage
+	.get(async (req, res) => {
+		// Get the trending games
+		let trendingGameList = null;
+		try {
+			// Get the trending games
+			trendingGameList = await gamesData.getAllTrending();
 
-  .get(async (req, res) => {
-    let removedGames;
-    // try {
-    //   removedGames = await gamesData.removeAllPastGames();
-    // } catch (e) {
-    //   return res.status(500).json({ error: e });
-    // }
-    try {
-      console.log(req.session);
-      const trendingGameList = await gamesData.getAllTrending();
-      console.log(trendingGameList);
-      console.log("cookie details", req.session.user);
-      res.render("homepage", { title: "Homepage", trendingGameList });
-    } catch (e) {
-      return res.status(500).json({ error: e });
-    }
-  });
-router.post("/storeLocation", (req, res) => {
-  // Get the user's location data from the request body
-  const { latitude, longitude } = req.body;
+			// Limit the number of trending games to four
+			if (trendingGameList.length > 4) trendingGameList.length = 4;
 
-  // Store the user's location in the session
-  req.session.latitude = latitude;
-  req.session.longitude = longitude;
+			// Render the homepage with the limited trending games
+			res.render('homepage', { title: 'Homepage', trendingGameList });
+		} catch (e) {
+			return res.status(500).render('Error', { errorMessage: e });
+		}
+	});
 
-  // Send a response to the client
-  res.send("Location stored in session!");
+// Route to store the user's location
+router.post('/storeLocation', (req, res) => {
+	// Get the user's location data from the request body
+	const { latitude, longitude } = req.body;
+
+	// If the user session exists, update the user's location
+	if (checkUserSession(req, res)) {
+		req.session.latitude = latitude;
+		req.session.longitude = longitude;
+	}
+
+	// Send a response to the client
+	res.send('Location stored in session!');
 });
 
 // Serve the login page
-router.get("/login", (req, res) => {
-  res.render("login", { title: "Login" });
+router.get('/login', (req, res) => {
+	res.render('Login', { title: 'Login' });
 });
 
 // Serve the signup page
-router.get("/register", (req, res) => {
-  res.render("register", { title: "register" });
+router.get('/register', (req, res) => {
+	res.render('register', { title: 'register' });
 });
 
-router.get("/error", (req, res) => {
-  res.render("error", { title: "Error" });
+// Route to serve the error page
+router.get('/error', (req, res) => {
+	res.render('Error', { title: 'Error' });
 });
+
+// Export the router
 export default router;
